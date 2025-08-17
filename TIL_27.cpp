55553ca57353763349e477f99b37490e45285e65
Hanseo Lee <90747441+ilovehanseo@users.noreply.github.com>
2025-08-17 10:37:04 +0000

    숏코딩

---
+++ TIL_27.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<utility>
+#include<string>
+#include<limits>
+#include<cmath>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+
+using namespace std;
+
+template<typename T>
+using Vec = vector<T>;
+
+int main(){
+    
+
+
+    return 0;
+}
